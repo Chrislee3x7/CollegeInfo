d9e9403f324cfd182d022ddebf31378ead41ff60
Paaaark <hifipark0801@gmail.com>
2021-09-18 14:25:35 -0400

    new files

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ const search = document.getElementById("search_uni");
 search.addEventListener("keyup", function() {
     if (event.keyCode == 13) {
         console.log("Enter is pressed");
+        window.location.href = "college-profile.html";
     }
 })
 
-// Array including college names
-var universities = ["Purdue", "Purduz", "Purdzz", "Purzzz", "Puzzzz", "Pzzzzz"];
+// Loads universities from csv
+var universities = ["Purdue"];
+
+$.ajax({
+    url: 'example.csv',
+    dataType: 'text',
+}).done(fetchData);
+
+function fetchData(data) {
+    var allRows = data.split(/\r?\n|\r/);
+    for (var i = 0; i < allRows.length; ++i) {
+        if (i == 0) continue;
+        var row = allRows[i].split(',');
+        universities.push(row[1]);
+        console.log(row[1]);
+    }
+}
+
+
 /*
   @parameter inp: html object arr: array to get the list from
 */
@@ -15,6 +33,7 @@ function autocomplete(inp, arr) {
     var curr;
     inp.addEventListener("input", function() {
         var a, b, i, val = this.value;
+        var counter = 0;
 
         closeAllLists();
         if (!val) { return false; }
@@ -32,12 +51,14 @@ function autocomplete(inp, arr) {
         // Check if there are matching elements
         for (i = 0; i < arr.length; ++i) {
             if(arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                console.log("One of the condition satisfied");
+                counter++;
                 b = document.createElement("DIV");
                 b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
                 b.innerHTML += arr[i].substr(val.length);
-
-                b.innerHTML += "<input type='hidden' value='" + arr[i] > "'>";
+                
+                console.log("b was " + b.innerHTML);
+                //b.innerHTML += "<input type='hidden' value='" + arr[i] > "'>";
+                console.log("b became " + b.innerHTML);
 
                 b.addEventListener("click", function() {
                     inp.value = this.getElementByTagName("input")[0].value;
@@ -46,6 +67,15 @@ function autocomplete(inp, arr) {
 
                 a.appendChild(b);
              }
+            if (counter == 5) {
+                break;
+            }
+        }
+        
+        if (counter == 0) {
+            b = document.createElement("DIV");
+            b.innerHTML = "No universities found";
+            a.appendChild(b);
         }
     });
 }
